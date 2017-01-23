d46ab47276c0865b21da828de3d2cf3c76d43693
Laoshancun <laoshancun@foxmail.com>
2017-01-23 15:37:49 +0800

    Update docker-entrypoint.sh

--- 3.2/docker-entrypoint.sh
+++ 3.2/docker-entrypoint.sh
@@ -1,21 +1,48 @@
 #!/bin/bash
 set -e
 
-if [ "${1:0:1}" = '-' ]; then
-	set -- mongod "$@"
+map_uidgid() {
+  USERMAP_ORIG_UID=$(id -u ${MONGO_USER})
+  USERMAP_ORIG_GID=$(id -g ${MONGO_USER})
+  USERMAP_GID=${USERMAP_GID:-${USERMAP_GID:-$USERMAP_ORIG_GID}}
+  USERMAP_UID=${USERMAP_UID:-$USERMAP_ORIG_UID}
+  if [[ ${USERMAP_UID} != ${USERMAP_ORIG_UID} ]] || [[ ${USERMAP_GID} != ${USERMAP_ORIG_GID} ]]; then
+    echo "Mapping UID and GID for ${MONGO_USER}:${MONGO_USER} from ${USERMAP_ORIG_UID}:${USERMAP_ORIG_GID} to ${USERMAP_UID}:${USERMAP_GID}"
+    groupmod -g ${USERMAP_GID} ${MONGO_USER}
+    sed -ie "s/:${USERMAP_ORIG_UID}:${USERMAP_ORIG_GID}:/:${USERMAP_UID}:${USERMAP_GID}:/" /etc/passwd
+  fi
+}
+
+create_data_dir() {
+  mkdir -p ${MONGO_DATA_DIR}
+  chmod -R 0755 ${MONGO_DATA_DIR}
+  chown -R ${MONGO_USER}:${MONGO_USER} ${MONGO_DATA_DIR}
+}
+
+create_log_dir() {
+  mkdir -p ${MONGO_LOG_DIR}
+  chmod -R 0755 ${MONGO_LOG_DIR}
+  chown -R ${MONGO_USER}:${MONGO_USER} ${MONGO_LOG_DIR}
+}
+
+map_uidgid
+create_data_dir
+create_log_dir
+
+# allow arguments to be passed to mongod
+if [[ ${1:0:1} = '-' ]]; then
+  EXTRA_ARGS="$@"
+  set --
+elif [[ ${1} == mongod || ${1} == $(which mongod) ]]; then
+  EXTRA_ARGS="${@:2}"
+  set --
 fi
 
-# allow the container to be started with `--user`
-if [ "$1" = 'mongod' -a "$(id -u)" = '0' ]; then
-	chown -R mongodb /data/configdb /data/db
-	exec gosu mongodb "$BASH_SOURCE" "$@"
+# default behaviour is to launch mongod
+if [[ -z ${1} ]]; then
+  echo "Starting mongod..."
+  exec start-stop-daemon --start --chuid ${MONGO_USER}:${MONGO_USER} \
+    --exec $(which mongod) -- --config /etc/mongod.conf ${EXTRA_ARGS}
+else
+  exec "$@"
 fi
-
-if [ "$1" = 'mongod' ]; then
-	numa='numactl --interleave=all'
-	if $numa true &> /dev/null; then
-		set -- $numa "$@"
-	fi
-fi
-
-exec "$@"
